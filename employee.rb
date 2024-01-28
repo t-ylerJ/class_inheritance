b698e35384d3d986bdb360db5cf87a1a7492950d
Tyler Johnson <tylerj0937@gmail.com>
2024-01-28 02:58:45 -0800

    added base structure

---
+++ employee.rb
@@ -0,0 +1,13 @@
+class Employee
+
+  def initialize(name, salary, title, boss=nil)
+  @name = name
+  @salary = salary
+  @title = title 
+  @boss = boss
+  end
+
+
+
+
+end
---
+++ manager.rb
@@ -0,0 +1,17 @@
+require_relative "employee"
+
+class Manager < Employee
+  #class mgr extends employee
+
+  def initialize(name, salary, title, boss=nil)
+    super
+    @children = []
+    
+  
+  
+  
+  end
+
+
+
+end

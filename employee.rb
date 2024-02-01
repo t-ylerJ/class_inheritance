5c320f2383c0780d080a2c1f0143cf8fb61aef79
ChristopherJtrent <ChristopherJtrent@outlook.com>
2024-01-31 16:30:44 -0800

    commit changed files

--- employee.rb
+++ employee.rb
@@ -1,13 +1,17 @@
 class Employee
+  attr_reader :salary, :name, :title
+
 
   def initialize(name, salary, title, boss=nil)
-  @name = name
-  @salary = salary
-  @title = title 
-  @boss = boss
+    @name = name
+    @salary = salary
+    @title = title 
+    @boss = boss
   end
 
-
+  def bonus(multiplier)
+    @salary * multiplier
+  end   
 
 
 end
--- manager.rb
+++ manager.rb
@@ -2,16 +2,29 @@ require_relative "employee"
 
 class Manager < Employee
   #class mgr extends employee
+  attr_reader :salary, :name, :title, :children
 
   def initialize(name, salary, title, boss=nil)
     super
     @children = []
-    
-  
-  
-  
   end
 
+  def total_child_salary
+    # base case
+    # when child.respond_to?(:total_child_salary) is false
+    # recursive step
+    # self.salary + sum(children.total_child_salary)
+    @salary + @children.inject(0) do |acc, child|
+      #base case
+      unless child.respond_to?(:total_child_salary)
+        child.salary + acc
+      else
+        child.total_child_salary + acc
+      end
+    end
+  end
 
-
+  def bonus(multiplier)
+    total_child_salary * multiplier
+  end
 end
